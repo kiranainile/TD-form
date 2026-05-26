ae6942a7ecb0d69d4d05e7680c582be596b58f52
kiranainile <ainilekiran2011@gmail.com>
2026-05-26 16:57:24 +0530

    signup form project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'TD-form';
+  @ViewChild('signUp') signUp! :NgForm
+email: any;
+  
+
+  onSubmit (){
+    // if(this.signUp.invalid){
+    //   return
+    // }
+    console.log(this.signUp.value)
+    console.log(this.signUp)
+  }
 }
